4584c7df3b1979b3565d16e5533f6abae206901f
Alex Njagi <1992anjagi@gmail.com>
2023-03-13 14:14:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  let distance;
+  distance=199;
+  if (distance<=400){
+    console.log(data[0]= 'gives customers a free sample if the ride is less than or equal to 400 feet');
+  } else if (distance>2000){
+    console.log(data[0]= 'charges 30 dollars for a distance over 2000 feet');
+  } else if (distance>2500){
+    console.log(data[0]= 'does not allow rides over 2500 feet');
+  }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  let city;
+  const comment = city===city?"Ok, sounds good.":"No go.";
 }
 
 function switchOnCharmFromTip(){
